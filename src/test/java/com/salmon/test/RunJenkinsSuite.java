d1a18e3506b08a3e166360e1934c661d252e4ea9
sravuri23 <sravuri23@gmail.com>
2018-02-14 13:46:49 +0000

    Added log4 j properties file. Added screenshot hook after. Added extent report dependency for cucumber.

--- src/test/java/com/salmon/test/RunJenkinsSuite.java
+++ src/test/java/com/salmon/test/RunJenkinsSuite.java
@@ -1,7 +1,13 @@
 package com.salmon.test;
 
+import com.cucumber.listener.ExtentCucumberFormatter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @CucumberOptions(features = "src/test/resources/features/mercurysiteTest", tags = "@gui", monochrome = true, plugin = {
@@ -10,4 +16,16 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
         "rerun:target/cucumber-report/runjenkins/rerun.txt"},
         glue = "com.salmon.test")
 public class RunJenkinsSuite extends AbstractTestNGCucumberTests {
-}
+
+        @BeforeClass
+        public static void setup() {
+            ExtentCucumberFormatter.initiateExtentCucumberFormatter(new File("target/feature-overview/SatishAutomationReport.html"));
+            // Loads the extent config xml to customize on the report.
+            ExtentCucumberFormatter.loadConfig(new File("src/test/resources/extent-config.xml"));
+            // Also you can add system information using a hash map
+            Map systemInfo = new HashMap();
+            systemInfo.put("Cucumber version", "v1.2.5");
+            ExtentCucumberFormatter.addSystemInfo(systemInfo);
+        }
+
+    }
